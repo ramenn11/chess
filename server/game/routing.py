a061db29417c73f5be1dd5b568d9608bfcac7b9f
Raman <46654688+ramenn11@users.noreply.github.com>
2026-03-14 22:35:49 +0530

    Update websocket URL patterns for routing

--- server/game/routing.py
+++ server/game/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/(?P<game_id>\w+)/$', consumers.GameConsumer.as_asgi()),
-    re_path(r'ws/matchmaking/$', consumers.MatchmakingConsumer.as_asgi()),
-    re_path(r'ws/notifications/$', consumers.UserNotificationConsumer.as_asgi()), 
+    re_path(r'^ws/game/(?P<game_id>[^/]+)/$', consumers.GameConsumer.as_asgi()),
+    re_path(r'^ws/matchmaking/$', consumers.MatchmakingConsumer.as_asgi()),
+    re_path(r'^ws/notifications/$', consumers.UserNotificationConsumer.as_asgi()),
 ]
